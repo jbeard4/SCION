0976b7d7dd584f3c2c3574bd969bd0f15b0efcdf
Jacob Beard <jbeard4@cs.mcgill.ca>
2012-01-06 15:55:32 -0800

    Removed some scripts in bin/ that were not being maintained.

--- bin/run-tests-python.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Copyright (C) 2011 Jacob Beard
-# Released under GNU LGPL, read the file 'COPYING' for more information
-
-dn=`dirname $0`
-abspath=`cd $dn; pwd`
-basedir=`dirname $abspath`
-
-PYTHONPATH=$PYTHONPATH:$basedir/src/main/python/:$basedir/lib/py/:$basedir/src/main/xslt/ python $basedir/src/main/python/scxml/test/harness.py $* #src/test/*/*.json
--- bin/run-tests-rhino-mvn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Copyright (C) 2011 Jacob Beard
-# Released under GNU LGPL, read the file 'COPYING' for more information
-
-dn=`dirname $0`
-abspath=`cd $dn; pwd`
-basedir=`dirname $abspath`
-mvn -q -o -f $basedir/pom.xml exec:java -DscxmlArgs="$*"
