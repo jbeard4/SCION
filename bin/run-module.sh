06d8bde5d83683aaccbcca18ea1b0912bfc70a18
Jacob Beard <jbeard4@cs.mcgill.ca>
2012-01-09 20:06:53 -0500

    Got multi-process test framework up and running. All tests pass except for delayed-send/*, due to intricate timing issues communicating between processes. I'll filter these out in the next go-round.

---
+++ bin/run-module.sh
@@ -0,0 +1,13 @@
+# Copyright (C) 2011 Jacob Beard
+# Released under GNU LGPL, read the file 'COPYING' for more information
+
+dn=`dirname $0`
+abspath=`cd $dn; pwd`
+basedir=`dirname $abspath`
+
+moduleToRun=$1
+shift
+interpreter=${1-node}
+shift
+
+$interpreter $basedir/lib/js/r.js -lib $basedir/build/core/runner.js $basedir/build/core $moduleToRun $*
--- bin/run-multi-process-testing-framework.sh
+++ bin/run-multi-process-testing-framework.sh
@@ -4,4 +4,7 @@
 dn=`dirname $0`
 abspath=`cd $dn; pwd`
 
-$abspath/run-module.sh scxml/test/multi-process/server node $*
+$abspath/bin/run-module.sh scxml/test/multi-process/server node -projectDir $abspath -local -numLocalProcesses 8 -interpreters spidermonkey rhino jsc v8 -verbose -logFile out.txt
+
+#./bin/run-multi-process-testing-framework.sh -projectDir `pwd` -clientAddresses node-0{1..9} node-{10..19} node-{21..33} -interpreters v8 spidermonkey rhino -verbose -logFile out.txt -stopOnFail -clientModulePath `pwd`/src/main/coffeescript/scxml/test/multi-process/client.coffee
+
