7acad5ab70406d0ee75772e79819ba77cf0bdc3b
Jacob Beard <jbeard4@cs.mcgill.ca>
2012-01-26 16:10:52 -0500

    Exposed the annotator module on the scion object. Modified the node-repl demo to take advantage of this, so it uses this module programmatically (rather than from the bash script, before the node module is run). Also, modified the node-repl demo to be less verbose.

--- src/demo/node-repl/repl.js
+++ src/demo/node-repl/repl.js
@@ -2,28 +2,31 @@ var repl = require('repl'),
 	fs = require('fs'),
 	scion = require('scion');
 
-var annotatedScxmlJsonStr = fs.readFileSync(process.argv[2],'utf8');
+var scxmlJsonStr = fs.readFileSync(process.argv[2],'utf8');
 
 try{
-	var annotatedScxmlJson = JSON.parse(annotatedScxmlJsonStr);
+	var scxmlJson = JSON.parse(scxmlJsonStr);
 }catch(e){
 	console.error(e);
 	process.exit(1);
 }
 
+//3. annotate him programmatically
+var annotatedScxmlJson = scion.annotateScxmlJson(scxmlJson);
+
 //4. Convert the SCXML-JSON document to a statechart object model. This step essentially converts id labels to object references, parses JavaScript scripts and expressions embedded in the SCXML as js functions, and does some validation for correctness. 
 var model = scion.json2model(annotatedScxmlJson);
-console.log("model",model)
+//console.log("model",model)
 
 //5. Use the statechart object model to instantiate an instance of the statechart interpreter. Optionally, we can pass to the construct an object to be used as the context object (the 'this' object) in script evaluation. Lots of other parameters are available.
 var interpreter = new scion.NodeInterpreter(model);
-console.log("interpreter",interpreter);
+//console.log("interpreter",interpreter);
 
 //7. Call the start method on the new intrepreter instance to start execution of the statechart.
 interpreter.start();
 
 //let's test it by printing current state
-console.log("initial configuration",interpreter.getConfiguration());
+console.log(interpreter.getConfiguration().iter());
 
 var parseRE = /\((.*)\n\)/;
 
