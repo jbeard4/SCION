3f3a5b4c2d7288697f6bafe991a4814f897864f9
Jacob Beard <jbeard4@cs.mcgill.ca>
2012-06-15 20:50:12 -0400

    Added support for saving to mongodb.

--- test/research/node-server.js
+++ test/research/node-server.js
@@ -4,8 +4,11 @@ var fs = require('fs');
 var path = require('path');
 var common = require('./common');
 var http = require('http');
+var mongo = require('mongodb');
 
 var file = new Static.Server('.');
+
+var mongoClient = new mongo.Db('scion', new mongo.Server("127.0.0.1", 27017, {}));
 
 //start up the browser as a child process
 //save his pid
@@ -96,99 +99,115 @@ function startBrowser(browserName){
     browserProc = spawn(browserName,args);
 }
 
-var server = http.createServer(function (request, response) {
-    console.log("received request",request.url);
-    switch(request.url){
-        case "/test-please":
-            if(!currentGroup){  //we've just started up - our first request
-                currentGroup = testGroups.shift();
-            }
+function startServer(err,collection){
+    if(err){
+        throw err;
+    }
 
-            currentTest = currentGroup.tests.shift();
-            if(!currentTest){   //we're at the end of this group's tests
-                currentGroup = testGroups.shift();
-                console.log("starting new group",currentGroup.group);
-                currentTest = currentGroup.tests.shift();
-            } 
-
-            console.log("sending test to client",currentTest);
-            response.writeHead(200);
-            response.write(JSON.stringify(currentTest));
-            response.end();
-            break;
-        case "/result":
-
-            var s = "";
-            request.on("data",function(data){
-                s += data;
-            });
-            request.on("end",function(){
-                var result = JSON.parse(s);
-                currentTest.result = result;
-
-                //dump the test result to disk... or direct to database?
-                fs.writeFile( "data/" + currentTest.browser + "/" + (fileCount++) + ".json", JSON.stringify(currentTest,4,4), "utf8",function(err){
-                    if(err){
-                        throw err;
-                        return
-                    }
-
-                    //kill and restart the browser
-                    if(!currentGroup.tests.length){
-                        //if that was our last test in this group, kill the browser and start him up again
-                        console.log("killing browser");
-                        browserProc.kill();
-
-                        browserProc.on("exit",function(){
-                            if(response) response.end();
-                            if(testGroups.length){
-                                //start up browser again
-                                startBrowser(testGroups[0].browser);
-                            }else{
-                                console.log("that's all folks... wrapping up");
-                                //if that was our last test in our last group, then we're done, stop listening, and terminate gracefully
-                                server.close();
-                            }
-                        });
-                    }else{
-                        //otherwise, reply that everything is OK.
-                        response.writeHead(200);
-                    }
-
-                    response.end();
-                });
-            });
-            break;
-        case "/get-memory":
-            //use the saved pid to get memory usage
-            fs.readFile("/proc/" + browserProc.pid + "/status","utf8",function(err,contents){
-                if(err){
-                    response.writeHead(500);
-                    response.end();
-                    throw err;
-                    return;
+    var server = http.createServer(function (request, response) {
+        console.log("received request",request.url);
+        switch(request.url){
+            case "/test-please":
+                if(!currentGroup){  //we've just started up - our first request
+                    currentGroup = testGroups.shift();
                 }
 
-                var rss = parseInt(contents.match(/\nVmRSS:\s*([0-9]+) kB\n/)[1]);
-                var vsz = parseInt(contents.match(/\nVmSize:\s*([0-9]+) kB\n/)[1]);
+                currentTest = currentGroup.tests.shift();
+                if(!currentTest){   //we're at the end of this group's tests
+                    currentGroup = testGroups.shift();
+                    console.log("starting new group",currentGroup.group);
+                    currentTest = currentGroup.tests.shift();
+                } 
 
-                console.log("rss",rss,"vsz",vsz);
-
+                console.log("sending test to client",currentTest);
                 response.writeHead(200);
-                response.write(JSON.stringify({
-                    rss : rss,
-                    vsz : vsz
-                }));
+                response.write(JSON.stringify(currentTest));
                 response.end();
-            });
-            break;
-        default:
-            console.log("serving up file");
-            request.addListener('end', function () {
-                file.serve(request, response);
-            });
-            break;
-    }
-}).listen(8080);
+                break;
+            case "/result":
 
-startBrowser(testGroups[0].browser);   //kick him off
+                var s = "";
+                request.on("data",function(data){
+                    s += data;
+                });
+                request.on("end",function(){
+                    var result = JSON.parse(s);
+                    currentTest.result = result;
+
+                    //dump the test result to disk... or direct to database?
+                    console.log("inserting results into mongodb");
+                    collection.insert(currentTest, function(err, docs) {
+                        console.log("results inserted");
+
+                        if(err){
+                            console.log("error inserting doc",err);
+                        }
+
+                        //kill and restart the browser
+                        if(!currentGroup.tests.length){
+                            //if that was our last test in this group, kill the browser and start him up again
+                            console.log("killing browser");
+                            browserProc.kill();
+
+                            browserProc.on("exit",function(){
+                                if(response) response.end();
+                                if(testGroups.length){
+                                    //start up browser again
+                                    startBrowser(testGroups[0].browser);
+                                }else{
+                                    console.log("that's all folks... wrapping up");
+                                    //if that was our last test in our last group, then we're done, stop listening, and terminate gracefully
+                                    mongoClient.close();
+                                    server.close();
+                                }
+                            });
+                        }else{
+                            //otherwise, reply that everything is OK.
+                            response.writeHead(200);
+                        }
+
+                        response.end();
+                    });
+                });
+                break;
+            case "/get-memory":
+                //use the saved pid to get memory usage
+                fs.readFile("/proc/" + browserProc.pid + "/status","utf8",function(err,contents){
+                    if(err){
+                        response.writeHead(500);
+                        response.end();
+                        throw err;
+                        return;
+                    }
+
+                    var rss = parseInt(contents.match(/\nVmRSS:\s*([0-9]+) kB\n/)[1]);
+                    var vsz = parseInt(contents.match(/\nVmSize:\s*([0-9]+) kB\n/)[1]);
+
+                    console.log("rss",rss,"vsz",vsz);
+
+                    response.writeHead(200);
+                    response.write(JSON.stringify({
+                        rss : rss,
+                        vsz : vsz
+                    }));
+                    response.end();
+                });
+                break;
+            default:
+                console.log("serving up file");
+                request.addListener('end', function () {
+                    file.serve(request, response);
+                });
+                break;
+        }
+    }).listen(8080);
+
+    startBrowser(testGroups[0].browser);   //kick him off
+}
+
+
+
+mongoClient.open(function(err, p_client) {
+  mongoClient.collection('runs', startServer);
+});
+
