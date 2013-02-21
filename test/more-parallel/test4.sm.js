fab2c3dfa9863b23c37aa457f90da47dbaeb8bfd
Jacob Beard <jbeard4@cs.mcgill.ca>
2013-02-21 18:39:36 -0500

    Forgot to check in tests in more-parallel/  in previous commit.

---
+++ test/more-parallel/test4.sm.js
@@ -0,0 +1,65 @@
+//Generated on Thursday, February 21, 2013 18:37:34 by the SCION SCXML compiler
+
+function getDelayInMs(delayString){
+    if (!delayString) {
+        return 0;
+    } else {
+        if (delayString.slice(-2) === "ms") {
+            return parseFloat(delayString.slice(0, -2));
+        } else if (delayString.slice(-1) === "s") {
+            return parseFloat(delayString.slice(0, -1)) * 1000;
+        } else {
+            return parseFloat(delayString);
+        }
+    }
+}
+
+
+
+
+
+module.exports = {
+    "": "http://www.w3.org/2005/07/scxml",
+    "states": [
+        {
+            "id": "p",
+            "type": "parallel",
+            "states": [
+                {
+                    "id": "a",
+                    "transitions": [
+                        {
+                            "event": "t",
+                            "target": "a"
+                        }
+                    ],
+                    "states": [
+                        {
+                            "id": "a1"
+                        },
+                        {
+                            "id": "a2"
+                        }
+                    ]
+                },
+                {
+                    "id": "b",
+                    "transitions": [
+                        {
+                            "event": "t",
+                            "target": "b"
+                        }
+                    ],
+                    "states": [
+                        {
+                            "id": "b1"
+                        },
+                        {
+                            "id": "b2"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
