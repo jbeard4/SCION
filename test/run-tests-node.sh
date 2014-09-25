fde5634c59bec32820434d8bd05286b5d6c2fb22
Feyzo <feyzullahdaldal@gmail.com>
2014-09-26 01:03:16 +0300

    Fixed a bug

--- test/run-tests-node.sh
+++ test/run-tests-node.sh
@@ -10,30 +10,6 @@ sleep 1
 
 #run the client
 node scxml-test-framework/lib/test-client.js -v -r console \
-  scxml-test-framework/test/assign-current-small-step/*.scxml \
-  scxml-test-framework/test/atom3-basic-tests/*.scxml \
-  scxml-test-framework/test/basic/*.scxml \
-  scxml-test-framework/test/cond-js/*.scxml \
-  scxml-test-framework/test/default-initial-state/*.scxml \
-  scxml-test-framework/test/delayedSend/*.scxml \
-  scxml-test-framework/test/documentOrder/*.scxml \
-  scxml-test-framework/test/foreach/*.scxml \
-  scxml-test-framework/test/hierarchy/*.scxml \
-  scxml-test-framework/test/hierarchy+documentOrder/*.scxml \
-  scxml-test-framework/test/history/*.scxml \
-  scxml-test-framework/test/if-else/*.scxml \
-  scxml-test-framework/test/in/*.scxml \
-  scxml-test-framework/test/internal-transitions/*.scxml \
-  scxml-test-framework/test/more-parallel/*.scxml \
-  scxml-test-framework/test/multiple-events-per-transition/*.scxml \
-  scxml-test-framework/test/parallel/*.scxml \
-  scxml-test-framework/test/parallel+interrupt/*.scxml \
-  scxml-test-framework/test/script/*.scxml \
-  scxml-test-framework/test/script-src/*.scxml \
-  scxml-test-framework/test/scxml-prefix-event-name-matching/*.scxml \
-  scxml-test-framework/test/send-data/*.scxml \
-  scxml-test-framework/test/send-internal/*.scxml \
-  scxml-test-framework/test/targetless-transition/*.scxml \
   scxml-test-framework/test/w3c-ecma/test150.txml.scxml \
   scxml-test-framework/test/w3c-ecma/test151.txml.scxml \
   scxml-test-framework/test/w3c-ecma/test329.txml.scxml \
