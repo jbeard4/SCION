ea69417029c1496ebb078124fba31fa91b0fb8e6
Feyzo <feyzullahdaldal@gmail.com>
2014-09-29 16:55:55 +0300

    Added test329-2 to passing tests

--- test/run-tests-node.sh
+++ test/run-tests-node.sh
@@ -37,6 +37,7 @@ node scxml-test-framework/lib/test-client.js -v -r console \
   scxml-test-framework/test/w3c-ecma/test150.txml.scxml \
   scxml-test-framework/test/w3c-ecma/test151.txml.scxml \
   scxml-test-framework/test/w3c-ecma-modified/test329-1.txml.scxml \
+  scxml-test-framework/test/w3c-ecma-modified/test329-2.txml.scxml \
   scxml-test-framework/test/w3c-ecma/test525.txml.scxml
   #FIXME issue #29 - re-enable scxml-test-framework/test/actionSend/*.scxml \
 
