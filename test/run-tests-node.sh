3b395cc397027d49e0919df4d5354d3d017adafc
Feyzo <feyzullahdaldal@gmail.com>
2014-11-27 01:56:34 +0200

    Added new tests

--- test/run-tests-node.sh
+++ test/run-tests-node.sh
@@ -34,13 +34,28 @@ node scxml-test-framework/lib/test-client.js -v -r console \
   scxml-test-framework/test/send-data/*.scxml \
   scxml-test-framework/test/send-internal/*.scxml \
   scxml-test-framework/test/targetless-transition/*.scxml \
+  scxml-test-framework/test/w3c-ecma/test1.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test147.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test148.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test149.txml.scxml \
   scxml-test-framework/test/w3c-ecma/test150.txml.scxml \
   scxml-test-framework/test/w3c-ecma/test151.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test155.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test158.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test173.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test194.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test278.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test279.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test287.txml.scxml \
   scxml-test-framework/test/w3c-ecma-modified/test301.txml.scxml \
-  scxml-test-framework/test/w3c-ecma/test309.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test302.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test304.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test310.txml.scxml \
   scxml-test-framework/test/w3c-ecma/test312.txml.scxml \
   scxml-test-framework/test/w3c-ecma/test313.txml.scxml \
   scxml-test-framework/test/w3c-ecma/test314.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test318.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test319.txml.scxml \
   scxml-test-framework/test/w3c-ecma/test321.txml.scxml \
   scxml-test-framework/test/w3c-ecma/test322.txml.scxml \
   scxml-test-framework/test/w3c-ecma/test323.txml.scxml \
@@ -49,7 +64,29 @@ node scxml-test-framework/lib/test-client.js -v -r console \
   scxml-test-framework/test/w3c-ecma/test326.txml.scxml \
   scxml-test-framework/test/w3c-ecma-modified/test329-1.txml.scxml \
   scxml-test-framework/test/w3c-ecma-modified/test329-2.txml.scxml \
-  scxml-test-framework/test/w3c-ecma/test525.txml.scxml
+  scxml-test-framework/test/w3c-ecma/test332.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test335.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test337.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test339.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test376.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test378.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test387.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test396.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test401.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test403b.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test404.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test405.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test407.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test436.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test444.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test445.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test448.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test449.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test451.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test505.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test525.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test550.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test551.txml.scxml
   #FIXME w3c-ecma-modified/test301 should kill the process not raise error.
   #FIXME issue #29 - re-enable scxml-test-framework/test/actionSend/*.scxml \
 
