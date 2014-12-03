1752e461f0daa95d1194d18db583b2a6510fec1b
Feyzo <feyzullahdaldal@gmail.com>
2014-12-03 14:28:06 +0200

    Added test 552 / 558 and updated test framework

--- test/run-tests-node.sh
+++ test/run-tests-node.sh
@@ -90,7 +90,9 @@ node scxml-test-framework/lib/test-client.js -v -r console \
   scxml-test-framework/test/w3c-ecma/test505.txml.scxml \
   scxml-test-framework/test/w3c-ecma/test525.txml.scxml \
   scxml-test-framework/test/w3c-ecma/test550.txml.scxml \
-  scxml-test-framework/test/w3c-ecma/test551.txml.scxml
+  scxml-test-framework/test/w3c-ecma/test551.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test552.txml.scxml \
+  scxml-test-framework/test/w3c-ecma/test558.txml.scxml
   #FIXME w3c-ecma-modified/test301 should kill the process not raise error.
   #FIXME issue #29 - re-enable scxml-test-framework/test/actionSend/*.scxml \
 
