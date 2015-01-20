7eca3b763e133c66009ecbe92d17927c357da0be
Feyzo <feyzullahdaldal@gmail.com>
2015-01-20 14:28:59 +0200

    Indentation fix

--- test/node-test-server.js
+++ test/node-test-server.js
@@ -4,89 +4,109 @@
 //clean up statechart
 
 var scxml = require('..'),
-    http = require('http');
+  http = require('http');
 
-var sessionCounter = 0, sessions = {}, timeouts = {}, timeoutMs = 5000, 
-    eventProcessorTypes = {
-        'scxml': {
-          location: 'http://www.w3.org/TR/scxml/#SCXMLEventProcessor'
-        },
-        'basichttp': {
-          location: 'http://www.w3.org/TR/scxml/#BasicHTTPEventProcessor'
-        },
-        'dom': {
-          location: 'http://www.w3.org/TR/scxml/#DOMEventProcessor'
-        }
-      };
+var sessionCounter = 0,
+  sessions = {},
+  timeouts = {},
+  timeoutMs = 5000,
+  eventProcessorTypes = {
+    'scxml': {
+      location: 'http://www.w3.org/TR/scxml/#SCXMLEventProcessor'
+    },
+    'basichttp': {
+      location: 'http://www.w3.org/TR/scxml/#BasicHTTPEventProcessor'
+    },
+    'dom': {
+      location: 'http://www.w3.org/TR/scxml/#DOMEventProcessor'
+    }
+  };
 
-function loadScxml(scxmlStr){
-}
+function loadScxml(scxmlStr) {}
 
-function cleanUp(sessionToken){
-    delete sessions[sessionToken];
+function cleanUp(sessionToken) {
+  delete sessions[sessionToken];
 }
 
 http.createServer(function (req, res) {
-    //TODO: set a timeout to clean up if we don't hear back for a while
-    var s = "";
-    req.on("data",function(data){
-        s += data;
-    });
-    req.on("end",function(){
-        var sessionToken;
-        try{
-            var reqJson = JSON.parse(s);
-            if(reqJson.load){
-                console.log("Loading new statechart",reqJson.load);
+  //TODO: set a timeout to clean up if we don't hear back for a while
+  var s = "";
+  req.on("data", function (data) {
+    s += data;
+  });
+  req.on("end", function () {
+    var sessionToken;
+    try {
+      var reqJson = JSON.parse(s);
+      if (reqJson.load) {
+        console.log("Loading new statechart", reqJson.load);
 
-                scxml.urlToModel(reqJson.load,function(err,result){
-                    //console.log('model',model);
-                    if(err){
-                        console.error(err.stack);
-                        res.writeHead(500, {'Content-Type': 'text/plain'});
-                        res.end(err.message);
-                    }else{
-                        var interpreter = new scxml.scion.Statechart(result.model, { name: result.name, sessionid: sessionCounter, ioprocessors: eventProcessorTypes });
+        scxml.urlToModel(reqJson.load, function (err, result) {
+          //console.log('model',model);
+          if (err) {
+            console.error(err.stack);
+            res.writeHead(500, {
+              'Content-Type': 'text/plain'
+            });
+            res.end(err.message);
+          } else {
+            var interpreter = new scxml.scion.Statechart(result.model, {
+              name: result.name,
+              sessionid: sessionCounter,
+              ioprocessors: eventProcessorTypes
+            });
 
-                        var sessionToken = sessionCounter;
-                        sessionCounter++;
-                        sessions[sessionToken] = interpreter; 
+            var sessionToken = sessionCounter;
+            sessionCounter++;
+            sessions[sessionToken] = interpreter;
 
-                        var conf = interpreter.start(); 
+            var conf = interpreter.start();
 
-                        res.writeHead(200, {'Content-Type': 'application/json'});
-                        res.end(JSON.stringify({
-                            sessionToken : sessionToken,
-                            nextConfiguration : conf
-                        }));
+            res.writeHead(200, {
+              'Content-Type': 'application/json'
+            });
+            res.end(JSON.stringify({
+              sessionToken: sessionToken,
+              nextConfiguration: conf
+            }));
 
-                        timeouts[sessionToken] = setTimeout(function(){cleanUp(sessionToken);},timeoutMs);  
-                    }
-                });
+            timeouts[sessionToken] = setTimeout(function () {
+              cleanUp(sessionToken);
+            }, timeoutMs);
+          }
+        });
 
-            }else if(reqJson.event && (typeof reqJson.sessionToken === "number")){
-                console.log("sending event to statechart",reqJson.event);
-                sessionToken = reqJson.sessionToken;
-                var nextConfiguration = sessions[sessionToken].gen(reqJson.event);
-                console.log('nextConfiguration',nextConfiguration);
-                res.writeHead(200, {'Content-Type': 'application/json'});
-                res.end(JSON.stringify({
-                    nextConfiguration : nextConfiguration
-                }));
+      } else if (reqJson.event && (typeof reqJson.sessionToken === "number")) {
+        console.log("sending event to statechart", reqJson.event);
+        sessionToken = reqJson.sessionToken;
+        var nextConfiguration = sessions[sessionToken].gen(reqJson.event);
+        console.log('nextConfiguration', nextConfiguration);
+        res.writeHead(200, {
+          'Content-Type': 'application/json'
+        });
+        res.end(JSON.stringify({
+          nextConfiguration: nextConfiguration
+        }));
 
-                clearTimeout(timeouts[sessionToken]);
-                timeouts[sessionToken] = setTimeout(function(){cleanUp(sessionToken);},timeoutMs);  
-            }else{
-                //unrecognized. send back an error
-                res.writeHead(400, {'Content-Type': 'text/plain'});
-                res.end("Unrecognized request.\n");
-            }
-        }catch(e){
-            console.error(e.stack);
-            console.error(e);
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end(e.message);
-        }
-    });
+        clearTimeout(timeouts[sessionToken]);
+        timeouts[sessionToken] = setTimeout(function () {
+          cleanUp(sessionToken);
+        }, timeoutMs);
+      } else {
+        //unrecognized. send back an error
+        res.writeHead(400, {
+          'Content-Type': 'text/plain'
+        });
+        res.end("Unrecognized request.\n");
+      }
+    } catch (e) {
+      console.error(e.stack);
+      console.error(e);
+      res.writeHead(500, {
+        'Content-Type': 'text/plain'
+      });
+      res.end(e.message);
+    }
+  });
 }).listen(42000, '127.0.0.1');
 
