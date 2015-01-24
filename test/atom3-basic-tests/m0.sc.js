3eb0568fdbdb9cabda67d83de024cac1dd609533
Jacob Beard <jake@jacobeanrnd.com>
2015-01-24 00:39:40 -0500

    Refactoring: changed internal property 'type' to '$type' so as not to conflict with SCXML @type attribute on <history>, <transition> and <send> elements.

--- test/atom3-basic-tests/m0.sc.js
+++ test/atom3-basic-tests/m0.sc.js
@@ -46,7 +46,7 @@ module.exports = (function(_x,_sessionid,_name,_ioprocessors,In){
     return {
         "ns0": "http://www.w3.org/2005/07/scxml",
         "name": "root",
-        "type": "scxml",
+        "$type": "scxml",
         "states": [
             {
                 "id": "A",
