c5cf4a69fd6187a58efe88b06669a1cf7849bb01
Jacob Beard <jake@jacobeanrnd.com>
2015-07-05 18:01:10 +0200

    Works

--- device/main.js
+++ device/main.js
@@ -5,58 +5,57 @@ var EventSource = require('eventsource');
 
 console.log('MRAA Version: ' + m.getVersion()); //write the mraa version to the console
 
-var SCXML_URL = process.env.SCXML_URL || 'http://192.168.0.4:8002/api/v1/morse.scxml/_singleton';
+module.exports = function init(swagger, instanceId){
+  var myLed = new m.Gpio(3); //LED hooked up to digital pin 13 (or built in pin on Galileo Gen1 & Gen2)
+  myLed.dir(m.DIR_OUT); //set the gpio direction to output
 
-var myLed = new m.Gpio(3); //LED hooked up to digital pin 13 (or built in pin on Galileo Gen1 & Gen2)
-myLed.dir(m.DIR_OUT); //set the gpio direction to output
+  // Create the button object using GPIO pin 0
+  var button = new groveSensor.GroveButton(2);
 
+  // Read the input and print, waiting one second between readings
+  var previousButtonState, currentButtonState;
+  function readButtonValue() {
+    currentButtonState = button.value();
+    
+    if(currentButtonState !== previousButtonState){
+      var eventName = currentButtonState ? 'device.press' : 'device.release';
 
-// Create the button object using GPIO pin 0
-var button = new groveSensor.GroveButton(2);
-
-// Read the input and print, waiting one second between readings
-var previousButtonState, currentButtonState;
-function readButtonValue() {
-  currentButtonState = button.value();
-	
-  if(currentButtonState !== previousButtonState){
-    var eventName = currentButtonState ? 'device.press' : 'device.release';
-    console.log('eventName ',eventName ); 
-    request({
-     url : SCXML_URL,
-     method : 'POST',
-     json : {
-       name : eventName
-     }
-    }, function(err, res, body){
-      console.log('response body',body);
+      swagger.apis.default.sendEvent(
+        {  
+          InstanceId: instanceId,
+          Event: {name : eventName }
+        }, function (data) {
+          console.log('response body',body);
+          setTimeout(readButtonValue,100);
+          previousButtonState = currentButtonState;
+          myLed.write(currentButtonState);
+        }, function (data) {
+          console.log('error response');
+        });
+    }else{
       setTimeout(readButtonValue,100);
       previousButtonState = currentButtonState;
-      myLed.write(currentButtonState);
-    });
-  }else{
-    setTimeout(readButtonValue,100);
-    previousButtonState = currentButtonState;
+    }
   }
-}
 
-// Load lcd module on I2C
-var LCD = require('jsupm_i2clcd');
+  // Load lcd module on I2C
+  var LCD = require('jsupm_i2clcd');
 
-// Initialize Jhd1313m1 at 0x62 (RGB_ADDRESS) and 0x3E (LCD_ADDRESS) 
-var myLcd = new LCD.Jhd1313m1 (0, 0x3E, 0x62);
+  // Initialize Jhd1313m1 at 0x62 (RGB_ADDRESS) and 0x3E (LCD_ADDRESS) 
+  var myLcd = new LCD.Jhd1313m1 (0, 0x3E, 0x62);
 
-var es = new EventSource(SCXML_URL + '/_changes');
-var buffer = '';
-myLcd.write(buffer);  
-es.on('character',function(e){
-  console.log('character',e);	
-  var c = JSON.parse(e.data);
-  buffer += c;
-  myLcd.write(c);  
-});
-es.onerror = function() {
-  console.log('ERROR!');
+  var es = new EventSource(SCXML_URL + '/_changes');
+  var buffer = '';
+  myLcd.write(buffer);  
+  es.on('character',function(e){
+    console.log('character',e);	
+    var c = JSON.parse(e.data);
+    buffer += c;
+    myLcd.write(c);  
+  });
+  es.onerror = function() {
+    console.log('ERROR!');
+  };
+
+  readButtonValue();
 };
-
-readButtonValue();
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var microexpresscion = require('microexpresscion'),
     SwaggerClient = require('swagger-client');
 
 var PORT = process.env.PORT || 8888;
+var INSTANCEID = '_singleton';
 
 //start the server programmatically
 microexpresscion.initExpress({ port: PORT, pathToModel : __dirname + '/build/morse.scxml' }, function (err, express) {
@@ -13,10 +14,23 @@ microexpresscion.initExpress({ port: PORT, pathToModel : __dirname + '/build/mor
       url: 'http://localhost:' + PORT + '/smaas.json',
       success: function(){
         swagger.apis.default.createNamedInstance(
-          { InstanceId: '_singleton' },
+          { InstanceId: INSTANCEID },
           function onInstanceSuccess (data) {
             console.log('Created singleton instance:', data.headers.location);
-            //TODO: listen to hardware events
+
+            //initialize the instance
+            swagger.apis.default.sendEvent(
+              {  
+                InstanceId: INSTANCEID,
+                Event: {name : 'system.start'}
+              }, function (response) {
+                console.log('data',response.data);
+                //TODO: it would be good to mock this interface when we are not running on a real device
+                //var initDevice = require('./device/main');
+                //initDevice(swagger, INSTANCEID);
+              }, function (data) {
+                console.log('error response',data);
+              });
           },
           function onInstanceError (data) {
             console.log('Error on instance creation', data.data.toString());
