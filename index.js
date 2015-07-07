c55592ffae2c508e42e97e131fbc13157342af16
Jacob Beard <jake@jacobeanrnd.com>
2015-07-07 18:31:32 +0200

    Added HTML test UI

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 #!/usr/bin/env node
 'use strict';
 
+var path = require('path');
 var microexpresscion = require('microexpresscion'),
     SwaggerClient = require('swagger-client');
+
+var express = require('express');
 
 var PORT = process.env.PORT || 8888;
 var INSTANCEID = '_singleton';
 var hostUrl = 'http://localhost:' + PORT;
 
 //start the server programmatically
-microexpresscion.initExpress({ port: PORT, pathToModel : __dirname + '/build/morse.scxml' }, function (err, express) {
-  express.app.listen(PORT, function(){
+microexpresscion.initExpress({ port: PORT, pathToModel : __dirname + '/build/morse.scxml' }, function (err, opts) {
+  console.log('opts',opts);
+  opts.app.get('/foo',function(req,res){res.send('works')});
+  opts.app.use('/app',express.static(path.join(__dirname, './app')));
+  opts.app.listen(PORT, function(){
     //use the swagger js client library to set up singleton instance
     var swagger = new SwaggerClient({
       url: hostUrl + '/smaas.json',
@@ -27,9 +33,10 @@ microexpresscion.initExpress({ port: PORT, pathToModel : __dirname + '/build/mor
                 Event: {name : 'system.start'}
               }, function (response) {
                 console.log('data',response.data);
-                //TODO: it would be good to mock this interface when we are not running on a real device
-                var initDevice = require('./device/main');
-                initDevice(swagger, INSTANCEID, hostUrl);
+                if(process.env.EDISON){
+                  var initDevice = require('./device/main');
+                  initDevice(swagger, INSTANCEID, hostUrl);
+                }
               }, function (data) {
                 console.log('error response',data);
               });
