001ce2c252abbbe9bdace941cc3a8c33fb214bee
Jacob Beard <jake@jacobeanrnd.com>
2015-08-13 10:08:03 -0400

    Cleanup

--- trie-to-scxml.js
+++ trie-to-scxml.js
@@ -4,8 +4,7 @@
 
 var trie = require('./morse-to-trie');
 var fs = require('fs');
-
-var eventProcessorScxml = fs.readFileSync(__dirname + '/components/event_processor.scxml','utf8');
+var ejs = require('ejs');
 
 var states = [];
 
@@ -27,33 +26,8 @@ walk(trie);
 //console.log(JSON.stringify(states,4,4))
 
 //now generate SCXML
-var scxmlString = 
-  '<?xml version="1.0" encoding="UTF-8"?>' + 
-  '<scxml xmlns="http://www.w3.org/2005/07/scxml" name="morse" datamodel="ecmascript" version="1.0">' + 
-    '<parallel id="main">' +
-      '<state id="event_processor">' +
-        eventProcessorScxml +
-      '</state>' +
-      '<state id="parsing_state">' +
-        '<transition target="parsing_state" event="reset"/>' + 
-        states.map(function(state){
-          return '<state id="' + state.id + '">' + 
-            (
-              state.emit ? 
-                '<onentry><send type="https://github.com/jbeard4/SCION#publish" target="scxml://publish" event="character" contentexpr="\'' + state.emit + '\'"></send></onentry>' +
-                '<transition target="idle"/>' //go back to start 
-                : '' ) + 
-            state.transitions.map(function(t){
-              return '<transition target="' + t.target + '" event="' + t.event + '"/>' 
-            }).join('') + 
-            (
-              state.id === 'idle' ? 
-                '<transition target="idle" event="long_pause"><send type="https://github.com/jbeard4/SCION#publish" event="character" contentexpr="\' \'"></send></transition>' 
-                : '' ) + 
-        '</state>' 
-        }).join('') + 
-      '</state>' +
-    '</parallel>' +
-  '</scxml>';
+var filename = './components/index.scxml.ejs';
+var str = fs.readFileSync(filename, 'utf8'); 
+var scxmlString = ejs.render(str, {states : states}, {filename : filename});
 
 console.log(scxmlString);
