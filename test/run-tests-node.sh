6d390691111b7dff503ccc9e4af03bdb27bc6036
Jacob Beard <jake@minnow.io>
2015-09-27 11:57:05 -0400

    Changed run-tests-node

--- test/run-tests-node.sh
+++ test/run-tests-node.sh
@@ -126,7 +126,7 @@ if [ "$status" = '0' ]; then echo SUCCESS; else echo FAILURE; exit $status; fi;
 
 # test platform-tests/node
 # TODO: capture and incorporate return value
-./node_modules/nodeunit/bin/nodeunit platform-tests/node/*/runner.js 
+./node_modules/nodeunit/bin/nodeunit --reporter junit platform-tests/node/*/runner.js 
 
 status=$?
 
