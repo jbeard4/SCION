a147d9250ed0ada7f88885c7929a812ba9244f25
Jacob BEard <jacob@localhost.localdomain>
2016-02-20 13:04:07 -0500

    Added initial animation system.

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var options = {
       edgeRouting: "ORTHOGONAL",
       cycleBreaking: "INTERACTIVE",
       nodeLayering: "INTERACTIVE",
-    },
+    }/*,
     order: {
       algorithm: "de.cau.cs.kieler.klay.layered",
       spacing: 10,
@@ -42,56 +42,89 @@ var options = {
       cycleBreaking: "INTERACTIVE",
       nodeLayering: "INTERACTIVE",
       crossMin: "INTERACTIVE",
-    }
+    }*/
 };
 
 
 var s,
-  graphNodeToSnapNodeIdMap,
-  snapNodeToGraphNodeIdMap;
+    ANIM_DURATION = 250;
     
 function init(klayExample){
 
-  // If you want to create new surface just provide dimensions
-  // like s = Snap(800, 600);
   s = Snap("#svg");
-  // Lets create big circle in the middle:
-  //var bigCircle = s.circle(150, 150, 100);
-  // By default its black, lets change its attributes
-  //bigCircle.attr({
-  //    fill: "#bada55",
-  //    stroke: "#000",
-  //    strokeWidth: 5
-  //});
-  // Now lets create another small circle:
-  //var smallCircle = s.circle(100, 150, 70);
 
-  console.log('$klay',$klay,'klayExample',klayExample );
+  var select = $('#select-layout');
+  select.on('change',layout); 
+  select.html(Object.keys(options).map(function(k){return '<option value="' + k + '">' + k + '</option>';}).reduce(function(a,b){return a + b;},''));
 
-  graphNodeToSnapNodeIdMap = {};
-  snapNodeToGraphNodeIdMap = {};
+  function layout(){
 
-  var graph;
-  $klay.layout({
-    graph : klayExample,
-    options : options.auto,
-    success : function(g){ 
-      graph = g;
-    }
-  });
+    applyInitialCoordinates(klayExample);
 
-  render(graph);
+    var graph;
+    $klay.layout({
+      graph : klayExample,
+      options : options[select.val()],
+      success : function(g){ 
+        graph = g;
+      }
+    });
+
+    render(graph);
+  }
+
+  layout();
+}
+
+function clearBendpoints(parent) {
+  if (parent.edges) {
+    parent.edges.forEach(function(e) {
+      e.sourcePoint = {x:0, y:0};
+      e.targetPoint = {x:0, y:0};
+      e.bendPoints = [];
+    });
+  }
+  if (parent.children) {
+    parent.children.forEach(function(c) {
+      clearBendpoints(c);
+    });
+  }
+}
+
+function applyInitialCoordinates(parent) {
+  if (parent.children) {
+    parent.children.forEach(function(c) {
+   
+      if (c.properties && c.properties["de.cau.cs.kieler.position"]) {
+        var position = c.properties["de.cau.cs.kieler.position"].split(",");
+        c.x = parseInt(position[0]);
+        c.y = parseInt(position[1]);
+      } else {
+        c.x = 0;
+        c.y = 0;
+      }
+   
+      applyInitialCoordinates(c);
+    });
+  }
 }
 
 
 
 function render(graphRoot){
-  var group = s.group();
-  var rect = group.rect(graphRoot.x, graphRoot.y, graphRoot.width, graphRoot.height, 10, 10);
+  s.attr('viewBox','0 0 ' + graphRoot.width + ' ' + graphRoot.height);
+  if(!graphRoot._displayNode){
+    var group = s.group();
+    var rect = group.rect(graphRoot.x, graphRoot.y, graphRoot.width, graphRoot.height);
 
-  //graphNodeToSnapNodeIdMap[graphRoot.id] = rect;
-  graphRoot._displayNode = group;
-  group.addClass('compound');
+    graphRoot._displayNode = group;
+    group.addClass('node');
+    group.addClass('compound');
+  } else {
+    //animate
+    var rect = graphRoot._displayNode.select('rect');
+    rect.animate({x : graphRoot.x, y: graphRoot.y, width : graphRoot.width, height : graphRoot.height}, ANIM_DURATION);
+  }
 
   graphRoot.children.forEach(renderGraphNode.bind(this,graphRoot));
   graphRoot.edges.forEach(renderEdge.bind(this,graphRoot));
@@ -100,21 +133,22 @@ function render(graphRoot){
 
 function renderGraphNode(parentGraphNode, graphNode){
 
-  var group = parentGraphNode._displayNode.group();
-  var label = group.text();
-  var rect = group.rect(0, 0, graphNode.width, graphNode.height, 10, 10);
-  group.transform('t' + graphNode.x + ',' + graphNode.y); 
-  // By default its black, lets change its attributes
-  group.addClass(graphNode.children ? 'compound' : 'leaf');
+  if(!graphNode._displayNode){
+    var group = parentGraphNode._displayNode.group();
+    var rect = group.rect(0, 0, graphNode.width, graphNode.height);
+    var label = group.text(2.5, 6.5, graphNode.id);
+    label.attr('font-size','4px');
+    group.transform('t' + graphNode.x + ',' + graphNode.y); 
+    // By default its black, lets change its attributes
+    group.addClass('node');
+    group.addClass(graphNode.children ? 'compound' : 'leaf');
 
-  //TODO: create an SVG G element
-  //TODO: create an SVG text element
-
-  //TODO: render leaf nodes differently. 
-  
-  //render node
-  //graphNodeToSnapNodeIdMap[graphNode.id] = group;
-  graphNode._displayNode = group;
+    graphNode._displayNode = group;
+  } else {
+    //animate
+    graphNode._displayNode.animate({transform : 't' + graphNode.x + ',' + graphNode.y}, ANIM_DURATION);
+    graphNode._displayNode.select('rect').animate({width : graphNode.width, height : graphNode.height},ANIM_DURATION);
+  }
 
   if(graphNode.children){
     graphNode.children.forEach(renderGraphNode.bind(this,graphNode));
@@ -126,20 +160,29 @@ function renderGraphNode(parentGraphNode, graphNode){
 }
 
 function renderEdge(parentGraphNode, edge){
-  var points = 
-    [edge.sourcePoint.x, edge.sourcePoint.y].concat(
+  var d = 
+    'M' + edge.sourcePoint.x + ' ' + edge.sourcePoint.y + ' ' +
       (edge.bendPoints ? edge.bendPoints.map(function(bp){
-        return [ bp.x, bp.y ];
+        return 'L' +  bp.x + ' ' + bp.y + ' ';
       }).reduce(function(a,b){
-        return a.concat(b);  
-      },[]) : []).concat([edge.targetPoint.x, edge.targetPoint.y]));
-  console.log('points', points);
-  var path = parentGraphNode._displayNode.polyline(points);
+        return a + b;  
+      },'') : '') + 
+      ' L' + edge.targetPoint.x + ' ' + edge.targetPoint.y;
+  if(!edge._displayNode){
+    var path = parentGraphNode._displayNode.path(d);
+    path.addClass('link');
 
-  edge._displayNode = path;
-
-  path.addClass('link');
-
-  //graphNodeToSnapNodeIdMap[edge.id] = path;
-  
+    edge._displayNode = path;
+  } else {
+    if(edge._displayNode.numberOfItems == ((edge.bendPoints ? edge.bendPoints.length : 0) + 2)){
+      //animate
+      edge._displayNode.animate({d : path}, ANIM_DURATION)
+    } else {
+      //remove and rebuild
+      edge._displayNode.remove();
+      var path = parentGraphNode._displayNode.path(d);
+      path.addClass('link');
+      edge._displayNode = path;
+    }
+  }
 }
