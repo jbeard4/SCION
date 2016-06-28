d17dc650c6c7031b543d62b247088450e88b2cda
Jacob Beard <jake@jacobeanrnd.com>
2016-06-29 00:09:17 +0200

    Sauce Labs integration nearly working.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,12 @@
 module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
+  var browsers = [{
+    browserName: 'chrome',
+    version: '46',
+    platform: 'Mac'
+  }];
+
   grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'),
       uglify: {
@@ -38,11 +44,36 @@ module.exports = function(grunt) {
           src: ['test/harness/common/setup-nodeunit-tests.js'],
           dest: 'test/harness/browser/build/nodeunit-tests-bundle.js'
         }
+      },
+      connect: {
+        server: {
+          options: {
+            base: '',
+            port: 9999
+          }
+        }
+      },
+      'saucelabs-custom': {
+        all: {
+          options: {
+            urls: [
+              'http://127.0.0.1:9999/test/harness/browser/harness.html'
+            ],
+            browsers: browsers,
+            //build: process.env.TRAVIS_JOB_ID,
+            testname: 'custom tests',
+            throttled: 3,
+            sauceConfig: {
+              'video-upload-on-pass': false
+            }
+          }
+        }
       }
   });
 
   grunt.registerTask('build-tests', ['browserify:dev']);
   grunt.registerTask('test', ['build', 'build-tests', 'run-tests']);
-  grunt.registerTask('run-tests', ['nodeunit']);
+  grunt.registerTask('run-tests', ['nodeunit', 'run-browser-tests' ]);
+  grunt.registerTask('run-browser-tests', ['connect', 'saucelabs-custom' ]);
   grunt.registerTask('build', ['babel', 'uglify']);
 };
