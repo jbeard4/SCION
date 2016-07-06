104e8a3d0f86ade40cfc48d8a731dd7d17f0080d
Jacob Beard <jake@jacobeanrnd.com>
2016-07-06 08:07:18 -0400

    Updated supported browsers.

--- grunt/browsers.js
+++ grunt/browsers.js
@@ -10,40 +10,43 @@ function addBrowser(browserName, platform, version){
   });
 }
 
+var defaultFF = [4, 10, 20, 30, 40, 46, 'dev', 'beta'],
+    defaultChrome = [26, 36, 51, 'dev', 'beta'];
+
 addBrowser('internet explorer', 'Windows 10', 11);
 addBrowser('MicrosoftEdge', 'Windows 10', '13.10586');
-_.range(26, 51 + 1).forEach(addBrowser.bind(this, 'chrome', 'Windows 10'));
-_.range(4, 46 + 1).forEach(addBrowser.bind(this, 'firefox', 'Windows 10'));
+defaultChrome.forEach(addBrowser.bind(this, 'chrome', 'Windows 10'));
+defaultFF.forEach(addBrowser.bind(this, 'firefox', 'Windows 10'));
 
-_.range(8, 11 + 1).forEach(addBrowser.bind(this, 'internet explorer', 'Windows 7'));
-_.range(26, 51 + 1).forEach(addBrowser.bind(this, 'chrome', 'Windows 7'));
-_.range(11, 12 + 1).forEach(addBrowser.bind(this, 'opera', 'Windows 7'));
-_.range(4, 46 + 1).forEach(addBrowser.bind(this, 'firefox', 'Windows 7'));
+[8, 9, 10, 11].forEach(addBrowser.bind(this, 'internet explorer', 'Windows 7'));
+defaultChrome.forEach(addBrowser.bind(this, 'chrome', 'Windows 7'));
+[11, 12].forEach(addBrowser.bind(this, 'opera', 'Windows 7'));
+defaultFF.forEach(addBrowser.bind(this, 'firefox', 'Windows 7'));
 addBrowser('safari', 'Windows 7', 5);
 
 addBrowser('internet explorer', 'Windows XP', 8);
-_.range(26, 49 + 1).forEach(addBrowser.bind(this, 'chrome', 'Windows XP'));
-_.range(11, 12 + 1).forEach(addBrowser.bind(this, 'opera', 'Windows XP'));
-_.range(4, 45 + 1).forEach(addBrowser.bind(this, 'firefox', 'Windows XP'));
+[26, 36, 49].forEach(addBrowser.bind(this, 'chrome', 'Windows XP'));
+[11, 12].forEach(addBrowser.bind(this, 'opera', 'Windows XP'));
+[4, 10, 20, 30, 40, 45].forEach(addBrowser.bind(this, 'firefox', 'Windows XP'));
 
-_.range(26, 51 + 1).forEach(addBrowser.bind(this, 'chrome', 'OS X 10.11'));
-_.range(4, 46 + 1).forEach(addBrowser.bind(this, 'firefox', 'OS X 10.11'));
+defaultChrome.forEach(addBrowser.bind(this, 'chrome', 'OS X 10.11'));
+defaultFF.forEach(addBrowser.bind(this, 'firefox', 'OS X 10.11'));
 addBrowser('safari', 'OS X 10.11', 9);
 
-_.range(37, 51 + 1).forEach(addBrowser.bind(this, 'chrome', 'OS X 10.10'));
-_.range(32, 44 + 1).forEach(addBrowser.bind(this, 'firefox', 'OS X 10.10'));
+defaultChrome.forEach(addBrowser.bind(this, 'chrome', 'OS X 10.10'));
+[32, 42, 44].forEach(addBrowser.bind(this, 'firefox', 'OS X 10.10'));
 addBrowser('safari', 'OS X 10.10', 8);
 
-_.range(31, 51 + 1).forEach(addBrowser.bind(this, 'chrome', 'OS X 10.9'));
-_.range(4, 46 + 1).forEach(addBrowser.bind(this, 'firefox', 'OS X 10.9'));
+defaultChrome.forEach(addBrowser.bind(this, 'chrome', 'OS X 10.9'));
+defaultFF.forEach(addBrowser.bind(this, 'firefox', 'OS X 10.9'));
 addBrowser('safari', 'OS X 10.9', 7);
 
-_.range(27, 49 + 1).forEach(addBrowser.bind(this, 'chrome', 'OS X 10.8'));
-_.range(4, 46 + 1).forEach(addBrowser.bind(this, 'firefox', 'OS X 10.8'));
+[27, 30, 40, 49].forEach(addBrowser.bind(this, 'chrome', 'OS X 10.8'));
+defaultFF.forEach(addBrowser.bind(this, 'firefox', 'OS X 10.8'));
 addBrowser('safari', 'OS X 10.8', 6);
 
-_.range(26, 48 + 1).forEach(addBrowser.bind(this, 'chrome', 'Linux'));
-_.range(4, 45 + 1).forEach(addBrowser.bind(this, 'firefox', 'Linux'));
+[26, 30, 40, 48].forEach(addBrowser.bind(this, 'chrome', 'Linux'));
+[4, 10, 20, 30, 40, 45].forEach(addBrowser.bind(this, 'firefox', 'Linux'));
 addBrowser('opera', 'Linux', 12);
 
 module.exports = browsers;
