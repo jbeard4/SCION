864aede26c6287f2ee6125d1922b84bf6ac8b672
Jacob Beard <jake@jacobeanrnd.com>
2016-08-01 22:24:08 -0400

    Test are running and failing. Need to set up source maps to debug.

--- grunt/config/karma/karma.conf.js
+++ grunt/config/karma/karma.conf.js
@@ -2,9 +2,13 @@
 module.exports = function(config) {
   config.set({
     basePath: '../../../',
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine-jquery', 'jasmine'],
+    preprocessors: {
+      'dist/*.js': ['sourcemap']
+    },
     files : [
-      {pattern: 'test/scxml-test-framework/test/*/*.{scxml,json}}', watched: true, served: true, included: false},
+      {pattern: 'test/scxml-test-framework/test/*/*.scxml', watched: true, served: true, included: false},
+      {pattern: 'test/scxml-test-framework/test/*/*.json', watched: true, served: true, included: false},
       {pattern: 'dist/scxml.js.map', watched: true, served: true, included: false},
       'test/nodeunit-test-harness/harness/browser/lib/polyfill.js',
       'dist/scxml.js',
