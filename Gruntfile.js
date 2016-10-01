df92e689212082a76f1ff31a1e492ebb148892ea
Jacob Beard <jake@jacobeanrnd.com>
2016-10-01 11:54:42 -0400

    Moved tests-integration framework into viz/

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,7 @@ module.exports = function(grunt) {
       },
       open : {
         dev : {
-          path : 'http://127.0.0.1:3000/test-integration/test.html',
+          path : 'http://127.0.0.1:3000/test-integration/viz/test.html',
           app: 'Google Chrome'
         }
       },
