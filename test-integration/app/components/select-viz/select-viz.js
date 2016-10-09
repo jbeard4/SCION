5a9532557b91e66d769e6bb3193e239630b1ffec
Jacob Beard <jake@jacobeanrnd.com>
2016-10-09 00:33:26 -0400

    Save which tests are selected.

--- test-integration/app/components/select-viz/select-viz.js
+++ test-integration/app/components/select-viz/select-viz.js
@@ -4,16 +4,25 @@ angular.module('schviz2.components.selectViz', ['schviz2.service', 'schviz2.cons
   bindings: {
     allTests : '<'
     ,onUpdate : '<'
+    ,name : '<'
   }
 });
 
-function SelectVizController(klayOptions, allTests){
+function SelectVizController(klayOptions, allTests, $scope, $window){
 
   var $ctrl = this;
   $ctrl.layout = klayOptions.right;
   $ctrl.klayOptions = klayOptions;
   allTests.then(function(tests){
       $ctrl.allTests = tests;
-      $ctrl.testUrl = tests[0];
+      $ctrl.testUrl = $window.localStorage[$ctrl.name + '.testUrl']  ?  JSON.parse($window.localStorage[$ctrl.name + '.testUrl']) : tests[0];
+  });
+
+  
+  ['testUrl'].forEach(function(prop){
+    
+    $scope.$watch('$ctrl.' + prop,function(){
+      if($ctrl[prop]) $window.localStorage[$ctrl.name + '.' + prop] = JSON.stringify($ctrl[prop]);
+    });
   });
 }
