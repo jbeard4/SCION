151b087393d57f289eaf6f15d4b1b04bfca1ca61
Jacob Beard <jake@jacobeanrnd.com>
2017-02-20 13:08:30 -0500

    Tweak marker animation

--- src/renderers/react/index.tsx
+++ src/renderers/react/index.tsx
@@ -402,11 +402,11 @@ class GraphEdge extends React.Component<GraphEdgeProps, GraphEdgeAnimation> {
         if(props.edge.$type === 'hyperlink'){
           markers.push('none','none');  //SMIL will override the CSS class, believe it or not
         } else {
-          markers.push(this.state.marker.split(';').pop());
           var from, to;
           [from, to] = points.slice(points.length-2);
           var type = this._getBendpointDirection(from, to);
-          markers.push(`url(#${type})`);
+          var markerUrl = `url(#${type})`;
+          markers.push(markerUrl, markerUrl);
         }
         return markers.join(';');
       })(),   
