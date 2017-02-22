af09764b09dfe6c4a045f3748452242a4b6acf57
Jacob Beard <jake@jacobeanrnd.com>
2017-02-22 12:49:42 -0500

    Fix duplicate edgeId.

--- src/renderers/react/index.tsx
+++ src/renderers/react/index.tsx
@@ -320,7 +320,7 @@ class GraphNode extends React.Component<GraphNodeProps, GraphNodeAnimation> {
       }
       {
         myEdges.map((edge,i) => (
-          <GraphEdge edge={edge} key={edge.id}/>
+          <GraphEdge edge={edge} key={`${edge.id}_${i}`}/>
         ))
       }
     </g>;
