94a2ad99843133b6f1fa1baeb468865f4aae2719
Jacob Beard <jake@jacobeanrnd.com>
2017-03-13 14:04:07 -0400

    Initial commit. Working well.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const optimist = require('optimist')
 const electron = require('electron')
 // Module to control application life.
 const app = electron.app
@@ -7,37 +8,43 @@ const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
 
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let scxmlWindows = {};
 
-function createWindow () {
+function createWindow (scxmlFile) {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  let window = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  window.loadURL(`file://${__dirname}/index.html?${scxmlFile}`)
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  window.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  window.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    scxmlWindows[scxmlFile] = null;
   })
+
+  scxmlWindows[scxmlFile] = window;
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', function(){
+  const scxmlFilesToOpen = optimist.argv._; 
+  console.log('scxmlFilesToOpen ', scxmlFilesToOpen );
+  
+  scxmlFilesToOpen.
+    filter( (file) => !scxmlWindows[file] ).
+    forEach( createWindow ); 
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -51,9 +58,6 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
 })
 
 // In this file you can include the rest of your app's specific main process
