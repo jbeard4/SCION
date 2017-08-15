ce5228d7a26aab00586291554515a2e837293a8b
Jacob Beard <jake@jacobeanrnd.com>
2017-08-15 19:28:09 -0400

    Initial example of dialogs based on invoked sessions.

---
+++ examples/basics-menus/app.js
@@ -0,0 +1,70 @@
+const util = require('util');
+const builder = require('botbuilder');
+const restify = require('restify');
+const scxml = require('scxml');
+let fnModel;
+
+const sessionStore = {};
+
+// Setup Restify Server
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat bot and listen to messages
+let connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+server.post('/api/messages', connector.listen());
+connector.onEvent(function(events){
+  events.forEach(processEvent)
+})
+
+scxml.pathToModel('./app.scxml', function(err, model){
+  if(err) throw err;
+  model.prepare(function(err, fn){
+    if(err) throw err;
+    fnModel = fn;
+  },{console : console, util : require('util')});
+});
+
+function processEvent(event){
+  //console.log(util.inspect(event, { depth: null }));
+  let interpreter = lazyInitSession(event)
+  let scxmlEvent = { name : event.type, data: event};
+  let configuration = interpreter.gen(scxmlEvent);
+}
+
+function lazyInitSession(event){
+  const id = event.address.conversation.id;
+  if(!sessionStore[id]){
+    let messageBuffer = [];
+    let interpreter = sessionStore[id] = new scxml.scion.Statechart(fnModel, {_sessionid : id, params : { messageBuffer } });
+    initSession(messageBuffer, interpreter);
+    interpreter.on('onInvokedSessionInitialized', function(invokedInterpreter){
+      initSession(messageBuffer, invokedInterpreter);
+    });
+
+    interpreter.start();
+  }
+  return sessionStore[id];
+}
+
+function initSession(messageBuffer, interpreter){
+  interpreter.on('onSmallStepBegin',function(){
+    console.log('onSmallStepBegin', messageBuffer);
+    //reset the buffer
+    messageBuffer.length = 0;     //this is the ugliest part of this - manipulating the SCXML datamodel from outside the state machine
+  });
+  interpreter.on('onSmallStepEnd',function(){
+    //flush the buffer
+    console.log('onSmallStepEnd', messageBuffer);
+    if(messageBuffer.length) {
+      connector.send(messageBuffer.slice(),function(err, addresses){
+        if(err) this.send(err);
+      }.bind(this));
+    }
+  });
+}
