3ef5ba70272356b24712a5760c29d2625090b35a
Jacob Beard <jake@jacobeanrnd.com>
2017-08-16 18:44:53 -0400

    Consolidate logging code. Added support for top-level final state.

--- examples/basics-menus/app.js
+++ examples/basics-menus/app.js
@@ -42,22 +42,37 @@ function lazyInitSession(event){
   const id = event.address.conversation.id;
   if(!sessionStore[id]){
     let messageBuffer = [];
-    let interpreter = sessionStore[id] = new scxml.scion.Statechart(fnModel, {_sessionid : id, params : { messageBuffer } });
-    initSession(messageBuffer, interpreter);
-    interpreter.start();
+    let interpreter = initNewInterpreter(id, messageBuffer);
   }
   return sessionStore[id];
 }
 
+function initNewInterpreter(id, messageBuffer){
+  let interpreter = sessionStore[id] = new scxml.scion.Statechart(fnModel, {sessionid : id, params : { messageBuffer } });
+  initSession(messageBuffer, interpreter);
+  interpreter.start();
+  return interpreter;
+}
+
+function log(interpreter, eventName, value){
+  console.log(path.parse(interpreter._model.docUrl).name, interpreter.opts.sessionid, eventName, value);
+}
+
 function initSession(messageBuffer, interpreter){
-  interpreter.on('onEntry',function(stateid){
-    console.log(path.parse(interpreter._model.docUrl).name, interpreter.opts.sessionid, 'onEntry', stateid);
+  interpreter.on('onExitInterpreter',function(lastEvent){
+    log(interpreter, 'onExitInterpreter', lastEvent.name);
+    let sessionId = interpreter.opts.sessionid;
+    if(sessionStore[sessionId]){
+      //unregister
+      interpreter.off();    //remove all event listeners
+      delete sessionStore[sessionId];
+      initNewInterpreter(sessionId, messageBuffer);    //TODO: provide a method to reset state machine
+    }
   })
-  interpreter.on('onExit',function(stateid){
-    console.log(path.parse(interpreter._model.docUrl).name, interpreter.opts.sessionid, 'onExit', stateid);
-  })
+  interpreter.on('onEntry',log.bind(this, interpreter, 'onEntry'));
+  interpreter.on('onExit',log.bind(this, interpreter, 'onExit'));
   interpreter.on('onBigStepEnd',function(){
-    console.log(path.parse(interpreter._model.docUrl).name, interpreter.opts.sessionid, 'onBigStepEnd', interpreter.getConfiguration());
+    log(interpreter, 'onBigStepEnd', interpreter.getConfiguration());
   })
   interpreter.on('onSmallStepBegin',function(){
     //reset the buffer
