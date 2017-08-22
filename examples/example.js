ac7fcac8a04229f214dd3aa3e02c4140cfe5d8b6
Jacob Beard <jake@jacobeanrnd.com>
2017-08-22 15:57:40 -0400

    Run through standard --fix

--- examples/example.js
+++ examples/example.js
@@ -5,14 +5,16 @@ var fs = require('fs'),
   sax = require('../lib/sax'),
   strict = sax.parser(true),
   loose = sax.parser(false, {trim: true}),
-  inspector = function (ev) { return function (data) {
+  inspector = function (ev) {
+    return function (data) {
       console.error('%s %s %j', this.line + ':' + this.column, ev, data)
-    }}
+    }
+  }
 
 sax.EVENTS.forEach(function (ev) {
   loose['on' + ev] = inspector(ev)
 })
-loose.onend = function () {
+loose.onend = (function () {
   console.error('end')
   console.error(loose)
 }
@@ -25,4 +27,4 @@ loose.onend = function () {
     xml = xml.substr(c)
     process.nextTick(arguments.callee)
   } else loose.close()
-})()
+}))()
