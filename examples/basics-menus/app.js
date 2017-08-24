e20383104b0db1418fecf9a5084c3ea0969616ae
Jacob Beard <jake@jacobeanrnd.com>
2017-08-24 13:13:30 -0400

    Switch to private npm modules

--- examples/basics-menus/app.js
+++ examples/basics-menus/app.js
@@ -1,7 +1,7 @@
 const util = require('util');
 const builder = require('botbuilder');
 const restify = require('restify');
-const scxml = require('scxml');
+const scxml = require('@jbeard/scxml');
 const path = require('path');
 let fnModel;
 
