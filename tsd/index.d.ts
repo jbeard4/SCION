c1f4b3ac20dfb3338bec907185b08077a694fed3
Jacob Beard <jake@jacobeanrnd.com>
2017-09-28 19:00:56 -0400

    Added generated API docs.

--- tsd/index.d.ts
+++ tsd/index.d.ts
@@ -2,13 +2,13 @@
 
 import * as scion from '@jbeard/scion-core';
 
-export interface SCModel {
-  prepare : (cb : (err : Error, fnModel : scion.FnModel) => any, executionContext? : any, hostContext? : any) => void;
+export interface ModelFactoryFactory {
+  prepare : (cb : (err : Error, modelFactory : scion.ModelFactory) => any, executionContext? : any, hostContext? : any) => void;
 }
 
-export function urlToModel(url : string, cb : (err : Error, model : SCModel) => any, context? : any);
-export function pathToModel(url : string, cb : (err : Error, model : SCModel) => any, context? : any);
-export function documentStringToModel(url : string, scxmlDocString : string, cb : (err : Error, model : SCModel) => any, context? : any);
+export function urlToModel(url : string, cb : (err : Error, modelFactoryFactory : ModelFactoryFactory) => any, context? : any);
+export function pathToModel(url : string, cb : (err : Error, modelFactoryFactory : ModelFactoryFactory) => any, context? : any);
+export function documentStringToModel(url : string, scxmlDocString : string, cb : (err : Error, modelFactoryFactory : ModelFactoryFactory) => any, context? : any);
 export namespace ext {
   export namespace compilerInternals {
     export const scxmlToScjson : (scxmlContents : string) => scion.SCState;
