5637a8a2603648f739c6f73bf4f1ecaa48c5963c
Jacob Beard <jake@jacobeanrnd.com>
2017-12-08 23:27:55 -0500

    Initial attempt at adding data grid.

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import Collapsible from 'react-collapsible';
+import ReactDataGrid = require('react-data-grid/packages/react-data-grid/index.js');
 import { bindActionCreators, ActionCreatorsMapObject } from 'redux';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
@@ -11,12 +13,56 @@ export interface AppProps {
 };
 
 
-class App extends React.Component<AppProps, {}> {
+export interface AppState {
+  minHeight : number;
+  rows : Array<Row>;
+};
+
+export interface Row {
+  name : string;
+  sessionid : number;
+  eventName : string;
+}
+
+export interface Column {
+  id : string;
+  name : string;
+  key : string;
+  width : number;
+  resizable : boolean;
+}
+
+
+class App extends React.Component<AppProps, AppState> {
 
   private rootElement : HTMLDivElement;
+  private reactDataGrid : ReactDataGrid;
+
+  constructor() {
+    super();
+    this.state = {
+      minHeight : 200,
+      rows : [
+        {
+          "name" : "foo",
+          "sessionid" : 1,
+          "eventName" : "bar"
+        }
+      ]
+    };
+  }
 
   render() {
     const { todos, actions } = this.props;
+
+    const columns = [
+        { id: "scxmlName", name: "SCXML Name", key: "name", width: 120, resizable: true },
+        //{ id: "docUrl", name: "URL", key: "docUrl", width: 120 },
+        { id: "sessionid", name: "Sesssionid", key: "sessionid", width: 120, resizable: true },
+        { id: "eventName", name: "Event Name", key: "eventName", width: 120, resizable: true },
+        
+    ];
+
     return (
       <div  style={{width:'100%',height:'100%'}} 
             ref={(e: HTMLDivElement) => { this.rootElement = e; }}>
@@ -25,35 +71,58 @@ class App extends React.Component<AppProps, {}> {
           <h1 id="title"></h1>
         </div>
         <div className="ui-layout-south">
-          <div id="tableContainer"></div>
+          <ReactDataGrid
+            ref={(node) => this.reactDataGrid = node}
+            enableCellSelect={true}
+            columns={columns}
+            rowGetter={this.getRowAt.bind(this)}
+            rowsCount={this.getSize()}
+            minHeight={this.state.minHeight-2}
+            //onGridRowsUpdated={this.handleGridRowsUpdated}
+            />
         </div>
         <div className="ui-layout-east">
-          <div id="infoContainer">
-            <div id="accordion">
-              <h3>Session Hierarchy</h3>
-              <div></div>
-              <h3>Input Event</h3>
-              <div id="events-tab"></div>
-              <h3>Datamodel</h3>
-              <div id="snapshot-tab"></div>
-              <h3>Datamodel Diff</h3>
-              <div id="diff-tab"></div>
-              <h3>Inner Queue</h3>
-              <div id="innerqueue-tab"></div>
-              <h3>Inner Queue Diff</h3>
-              <div id="innerqueue-diff-tab"></div>
-            </div>
-          </div>
+          <Collapsible trigger="Session Hierarchy">Session Hierarchy</Collapsible>
+          <Collapsible trigger="Input Event">Input Event</Collapsible>
+          <Collapsible trigger="Datamodel">Datamodel</Collapsible>
+          <Collapsible trigger="Datamodel Diff">Datamodel Diff</Collapsible>
+          <Collapsible trigger="Inner Queue">Inner Queue</Collapsible>
+          <Collapsible trigger="Inner Queue Diff">Inner Queue</Collapsible>
         </div>
       </div>
     );
   }
 
+  /*
+  handleGridRowsUpdated(e){
+    const { fromRow, toRow, updated } = e;
+    let rows = this.state.rows.slice();
+
+    for (let i = fromRow; i <= toRow; i++) {
+      rows = rows.update(i, r => r.merge(updated));
+    }
+
+    this.setState({ rows });
+  }
+  */
+
+  getRowAt(index) {
+    if (index < 0 || index > this.getSize()) {
+      return undefined;
+    }
+    return this.state.rows[index];
+  }
+
+  getSize(){
+    return this.state.rows.length;
+  }
+
   componentDidMount(){
-    window['jQuery'](this.rootElement).layout({ 
+    let myLayout = window['jQuery'](this.rootElement).layout({ 
       applyDefaultStyles: true, 
-      south__size: 200,
-      east__size: 300
+      south__size: this.state.minHeight,
+      east__size: 300,
+      south__onresize: (() => { console.log('myLayout.state.size', myLayout.state.south.size); this.setState({minHeight : myLayout.state.south.size })}),
     });
   }
 }
