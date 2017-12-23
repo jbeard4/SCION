2b4d1ae2168f71328c8e49c6719abfdbceebb803
Jacob Beard <jake@minnow.io>
2017-12-22 22:16:45 -0800

    commit

--- grunt/test-pairs.js
+++ grunt/test-pairs.js
@@ -1,10 +1,11 @@
 var grunt   = require('grunt');
+var path = require('path');
 
-var tests = grunt.file.expand(require('./scxml-tests.json'));
+var tests = grunt.file.expand(require('./scxml-tests.json').map( p => path.join(__dirname,'..',p)));
 
 var testPairs = tests.map(function(test){
   var filename = test.replace('\.scxml','.json');
-  return [test, require('../' + filename)];
+  return [test, require(filename)];
 });
 
 module.exports = testPairs;
