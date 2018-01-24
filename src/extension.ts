ad4ff758896da1c27315d12ccfb259f45332245e
Jacob Beard <jake@jacobeanrnd.com>
2018-01-24 16:52:18 -0500

    Animation works. Includes code from vscodeJupyter.

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,14 @@
 "use strict";
 
-import * as child_process from "child_process";
 import * as path from "path";
 import * as vscode from "vscode";
+import { TextDocumentContentProvider } from './resultView';
+import { Server } from './server';
 
-const extensionId = "graphviz-preview";
-const previewCommand = "graphviz.showPreview";
-const previewScheme = "graphviz-preview";
+const previewCommand = "scxml.showPreview";
+const previewScheme = "scxml-preview";
 
-// Utility functions.
-
-function getDotProgram(): string
-{
-    const configuration = vscode.workspace.getConfiguration(extensionId);
-    const dotPath = configuration.get<string>("dotPath");
-
-    if (dotPath === null)
-    {
-        return "dot";
-    }
-    else
-    {
-        return dotPath;
-    }
-}
-
-function getGraphvizPreviewUri(sourceUri: vscode.Uri)
+function getSchvizPreviewUri(sourceUri: vscode.Uri)
 {
     return (new vscode.Uri()).with({
         authority: "preview",
@@ -46,201 +29,42 @@ function getPreviewColumn(editor: vscode.TextEditor)
     }
 }
 
-function getSourceUri(previewUri: vscode.Uri): vscode.Uri
-{
-    return vscode.Uri.parse(previewUri.query);
-}
-
-function wrapSvgText(svgText: string): string
-{
-    return `<!DOCTYPE html>
-<html>
-    <head>
-        <title>Graphviz Preview</title>
-        <style>
-            html, body
-            {
-                height: 100%;
-            }
-
-            body
-            {
-                background-color: darkgray;
-                display: flex;
-                flex-flow: column;
-                margin: 0;
-                padding: 0;
-            }
-
-            #header
-            {
-                background-color: lightgray;
-                box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.62);
-                padding: 6px;
-                z-index: 1;
-            }
-
-            #main
-            {
-                display: flex;
-                flex: 1;
-                overflow: auto;
-            }
-
-            .zoom-identity {
-                flex: none;
-                margin: auto;
-            }
-
-            .zoom-fit {
-                height: 100%;
-                width: 100%;
-            }
-
-            .zoom-fit-100-percent {
-                margin: auto;
-                max-height: 100%;
-                max-width: 100%;
-            }
-        </style>
-        <script>
-            document.addEventListener('DOMContentLoaded', () =>
-            {
-                'use strict';
-
-                const main = document.getElementById('main');
-                const image = document.querySelector('#main > *');
-                const defaultZoomButtom = document.querySelector('input[name="zoom"][value="zoom-fit-100-percent"]');
-
-                function updateZoom()
-                {
-                    while (image.classList.length > 0)
-                    {
-                        image.classList.remove(image.classList[0]);
-                    }
-
-                    image.classList.add(this.value);
-                }
-
-                for (const button of document.querySelectorAll('input[name="zoom"]'))
-                {
-                    button.onchange = updateZoom;
-                }
-
-                defaultZoomButtom.checked = true;
-                defaultZoomButtom.onchange();
-            });
-        </script>
-    </head>
-    <body>
-        <header id="header">
-            <div>
-                <span>Zoom: </span>
-                <label><input name="zoom" type="radio" value="zoom-identity" /> 100 %</label>
-                <label><input name="zoom" type="radio" value="zoom-fit" /> Fit</label>
-                <label><input name="zoom" type="radio" value="zoom-fit-100-percent" /> Fit (at most 100 %)</label>
-            </div>
-        </header>
-        <main id="main">${svgText}</main>
-    </body>
-</html>
-`;
-}
-
-// The preview content provider class.
-
-class GraphvizPreviewContentProvider implements vscode.TextDocumentContentProvider
-{
-    private onDidChangeEventEmitter = new vscode.EventEmitter<vscode.Uri>();
-
-    public get onDidChange(): vscode.Event<vscode.Uri>
-    {
-        return this.onDidChangeEventEmitter.event;
-    }
-
-    public async provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): Promise<string>
-    {
-        const sourceUri = getSourceUri(uri);
-        const sourceDocument = await vscode.workspace.openTextDocument(sourceUri);
-        const sourceText = sourceDocument.getText();
-        const dotProgram = getDotProgram();
-
-        return new Promise<string>((resolve, reject) =>
-        {
-            const dotProcess = child_process.execFile(dotProgram,
-                ["-T", "svg"],
-                (error, stdout, stderr) =>
-                {
-                    if (error)
-                    {
-                        const codeProperty = "code";
-
-                        if (error[codeProperty] === "ENOENT")
-                        {
-                            reject(`File not found: ${dotProgram}`);
-                        }
-                        else
-                        {
-                            reject(stderr);
-                        }
-                    }
-                    else
-                    {
-                        resolve(wrapSvgText(stdout));
-                    }
-                });
-
-            token.onCancellationRequested((_) =>
-            {
-                try
-                {
-                    dotProcess.kill();
-                }
-                catch (_)
-                {
-                    return;
-                }
-            });
-
-            try
-            {
-                dotProcess.stdin.end(sourceText);
-            }
-            catch (_)
-            {
-                return;
-            }
-        });
-    }
-
-    public updatePreview(sourceUri: vscode.Uri)
-    {
-        this.onDidChangeEventEmitter.fire(getGraphvizPreviewUri(sourceUri));
-    }
-}
-
-// Extension interfaces.
-
 export function activate(context: vscode.ExtensionContext)
 {
-    const previewContentProvider = new GraphvizPreviewContentProvider();
+
+    let server;
+
+    const previewContentProvider = new TextDocumentContentProvider();
 
     context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(previewScheme,
         previewContentProvider));
 
     context.subscriptions.push(vscode.commands.registerCommand(previewCommand, () =>
     {
-        const activeTextEditor = vscode.window.activeTextEditor;
 
-        return vscode.commands.executeCommand("vscode.previewHtml",
-            getGraphvizPreviewUri(activeTextEditor.document.uri),
-            getPreviewColumn(activeTextEditor),
-            `Preview '${path.basename(activeTextEditor.document.uri.fsPath)}'`,
-            { allowScripts: true, allowSvgs: true });
-    }));
+        server = new Server();
+        server.start();
+        server.start().then(async port => {
+            console.log('server started', port);
+            previewContentProvider.ServerPort = port;
 
-    context.subscriptions.push(vscode.workspace.onDidChangeTextDocument((e) =>
-    {
-        previewContentProvider.updatePreview(e.document.uri);
-    }));
+            const activeTextEditor = vscode.window.activeTextEditor;
+
+            const sourceDocument = await vscode.workspace.openTextDocument(activeTextEditor .document.uri);
+            const sourceText = sourceDocument.getText();
+            server.buffer = sourceText;
+
+            return vscode.commands.executeCommand("vscode.previewHtml",
+                getSchvizPreviewUri(activeTextEditor.document.uri),
+                getPreviewColumn(activeTextEditor),
+                `Preview '${path.basename(activeTextEditor.document.uri.fsPath)}'`,
+                { allowScripts: true, allowSvgs: true });
+        })
+    }))
+
+    context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(async (e) => {
+        const sourceDocument = await vscode.workspace.openTextDocument(e.document.uri);
+        const sourceText = sourceDocument.getText();
+        server.sendUpdate(sourceText)
+    }))
 }
