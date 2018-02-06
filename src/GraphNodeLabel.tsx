0f08990ae7c4163240cf78dc4140f194d2a30368
Jacob Beard <jake@jacobeanrnd.com>
2018-02-05 17:29:49 -0800

    Animated x, y coordinates of GraphNodeLabel. Looks good.

--- src/GraphNodeLabel.tsx
+++ src/GraphNodeLabel.tsx
@@ -8,11 +8,37 @@ export interface GraphNodeLabelProps{
   disableAnimation? : boolean;
 }
 
-export default class GraphNodeLabel extends React.PureComponent<GraphNodeLabelProps, {}> {
+export interface GraphNodeLabelAnimation {
+  from : KGraphNode;
+  initialRender : boolean;
+}
+
+export default class GraphNodeLabel extends React.PureComponent<GraphNodeLabelProps, GraphNodeLabelAnimation> {
 
   svgTextElement : SVGTextElement;
+  animateXElement : SVGAnimationElement;
+  animateYElement : SVGAnimationElement;
   animateOpacityElement : SVGAnimationElement;
-  initialRender : boolean;
+
+  constructor(){
+    super();
+    this.state = { 
+      from : null,
+      initialRender : true
+    };
+  }
+
+  componentWillReceiveProps(props : GraphNodeLabelProps){
+    this.state = {
+      from : this.props.node,
+      initialRender : false
+    };
+  }
+
+  componentDidUpdate(){
+    //console.log('GraphLabel: componentDidUpdate',this.props.label);
+    this.animate();
+  }
 
   componentDidMount(){
     this.animate();
@@ -20,17 +46,36 @@ export default class GraphNodeLabel extends React.PureComponent<GraphNodeLabelPr
 
   private animate(){
     if(!this.props.disableAnimation) {
-      this.animateOpacityElement.beginElement()
+      [
+        this.animateXElement,
+        this.animateYElement,
+        this.animateOpacityElement 
+      ].forEach( animation => animation.beginElement() );
     }
   }
 
+  renderAsSingleLine(node){
+    return node.labels[0] && node.labels[0].text.trim().split('\n').length === 1;
+  }
+
+  toX(node){
+    return node.$type === 'contentContainer' && !this.renderAsSingleLine(node)  ? 0 : node.width / 2;
+  }
+
+  toY(node){
+    const isLeaf = !(node.children && node.children.length);
+    return node.$type === 'contentContainer' && !this.renderAsSingleLine(node)  ? 0 : (isLeaf ? node.height / 2 : constants.LEAF_NODE_PADDING_H);
+  }
+
   render(){
-    var isLeaf = !(this.props.node.children && this.props.node.children.length);
-    const renderAsSingleLine = this.props.node.labels[0] && this.props.node.labels[0].text.trim().split('\n').length === 1;
+    const fromX = this.toX(this.state.from || this.props.node);
+    const fromY = this.toY(this.state.from || this.props.node);
+    const toX = this.toX(this.props.node);
+    const toY = this.toY(this.props.node);
     return <text   
       ref={(e: SVGTextElement) => { this.svgTextElement = e; }}
-      x={this.props.node.$type === 'contentContainer' && !renderAsSingleLine  ? 0 : this.props.node.width / 2} 
-      y={this.props.node.$type === 'contentContainer' && !renderAsSingleLine  ? 0 : (isLeaf ? this.props.node.height / 2 : constants.LEAF_NODE_PADDING_H)}  
+      x={this.props.disableAnimation ? toX : undefined}
+      y={this.props.disableAnimation ? toY : undefined}
       visibility={this.props.isRoot ? 'hidden' : 'visible'}
       opacity={this.props.disableAnimation ? 1 : 0}
       >
@@ -39,7 +84,7 @@ export default class GraphNodeLabel extends React.PureComponent<GraphNodeLabelPr
         (
          this.props.node.$type === 'contentContainer' ? 
           (
-            renderAsSingleLine ? 
+            this.renderAsSingleLine(this.props.node) ? 
             this.props.node.labels[0].text.trim() : 
              this.props.node.labels[0].text.replace(/ /g,`\u00A0`)
                .split('\n')
@@ -49,14 +94,33 @@ export default class GraphNodeLabel extends React.PureComponent<GraphNodeLabelPr
       }
       {
         !this.props.disableAnimation && 
-          <animate attributeName="opacity" attributeType="XML"
-                   ref={(e: SVGAnimationElement) => { this.animateOpacityElement = e; }}
-                   fill="freeze" 
-                   begin="indefinite"
-                   className={constants.START}
-                   dur={constants.ANIM_DURATION} 
-                   from={this.initialRender ? 1 : 0} 
-                   to="1" />
+          [
+            <animate attributeName="x" attributeType="XML" fill="freeze" 
+                     begin="indefinite"
+                     key="0"
+                     from={fromX}
+                     to={toX}
+                     dur={constants.ANIM_DURATION}
+                     ref={(e: SVGAnimationElement) => { this.animateXElement = e; }}
+                     />,
+            <animate attributeName="y" attributeType="XML" fill="freeze" 
+                     begin="indefinite"
+                     key="1"
+                     from={fromY}
+                     to={toY}
+                     dur={constants.ANIM_DURATION}
+                     ref={(e: SVGAnimationElement) => { this.animateYElement = e; }}
+                     />,
+            <animate attributeName="opacity" attributeType="XML"
+                     key="2"
+                     ref={(e: SVGAnimationElement) => { this.animateOpacityElement = e; }}
+                     fill="freeze" 
+                     begin="indefinite"
+                     className={constants.START}
+                     dur={constants.ANIM_DURATION} 
+                     from={this.state.initialRender ? 0 : 1} 
+                     to="1" />
+          ]
       }
     </text>;
   }
