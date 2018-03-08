95d76d3df3c8d9a7660191cd414f3ab48238e66d
Jacob Beard <jake@jacobeanrnd.com>
2018-03-07 19:43:07 -0500

    Added support for embedded debugging client.

--- scripts/copy-scripts.sh
+++ scripts/copy-scripts.sh
@@ -1,5 +1,6 @@
-mkdir -p _attachments/node_modules/\@jbeard/scxml/dist/ _attachments/node_modules/babel-polyfill/dist/ _attachments/build
+mkdir -p _attachments/node_modules/\@jbeard/scxml/dist/ _attachments/node_modules/babel-polyfill/dist/ _attachments/build _attachments/node_modules/@jbeard/scion-scxml-debugger-middleware/dist/
 cp build/morse.scxml _attachments/build/
 cp index.html _attachments/
-cp node_modules/\@jbeard/scxml/dist/scxml.debug.js _attachments/node_modules/\@jbeard/scxml/dist/scxml.debug.js
+cp node_modules/\@jbeard/scxml/dist/scxml.js _attachments/node_modules/\@jbeard/scxml/dist/scxml.js
 cp node_modules/babel-polyfill/dist/polyfill.js _attachments/node_modules/babel-polyfill/dist/
+cp node_modules/@jbeard/scion-scxml-debugger-middleware/dist/client.js _attachments/node_modules/@jbeard/scion-scxml-debugger-middleware/dist/client.js
