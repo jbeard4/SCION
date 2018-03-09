405835b0e992687140bf6ea3d31a46d957d74e7e
Jacob Beard <jake@jacobeanrnd.com>
2018-03-09 13:14:28 -0500

    move these js file out onto the _design/app doc, so that they do not need to be saved in the project.

--- scripts/copy-scripts.sh
+++ scripts/copy-scripts.sh
@@ -1,6 +1,3 @@
-mkdir -p _attachments/node_modules/\@jbeard/scxml/dist/ _attachments/node_modules/babel-polyfill/dist/ _attachments/build _attachments/node_modules/@jbeard/scion-scxml-debugger-middleware/dist/
+mkdir -p _attachments/build 
 cp build/morse.scxml _attachments/build/
 cp index.html _attachments/
-cp node_modules/\@jbeard/scxml/dist/scxml.js _attachments/node_modules/\@jbeard/scxml/dist/scxml.js
-cp node_modules/babel-polyfill/dist/polyfill.js _attachments/node_modules/babel-polyfill/dist/
-cp node_modules/@jbeard/scion-scxml-debugger-middleware/dist/client.js _attachments/node_modules/@jbeard/scion-scxml-debugger-middleware/dist/client.js
