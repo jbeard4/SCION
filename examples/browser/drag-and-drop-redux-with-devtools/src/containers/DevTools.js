d9c64e229c6151a42cc954628155461be839a71c
Jacob Beard <jake@minnow.io>
2018-06-04 15:48:53 -0400

    Added support for ChartMonitor.

--- examples/browser/drag-and-drop-redux-with-devtools/src/containers/DevTools.js
+++ examples/browser/drag-and-drop-redux-with-devtools/src/containers/DevTools.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { createDevTools } from 'redux-devtools';
 import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
+import SliderMonitor from 'redux-slider-monitor';
+import ChartMonitor from 'redux-devtools-chart-monitor';
 
 export default createDevTools(
-  <DockMonitor toggleVisibilityKey='ctrl-h'
-               changePositionKey='ctrl-q'>
+  <DockMonitor 
+    changeMonitorKey='ctrl-m'
+    toggleVisibilityKey='ctrl-h'
+    changePositionKey='ctrl-q'>
+    <ChartMonitor />
     <LogMonitor />
+    <SliderMonitor keyboardEnabled />
   </DockMonitor>
 );
--- examples/browser/drag-and-drop-redux-with-devtools/src/reducers/index.js
+++ examples/browser/drag-and-drop-redux-with-devtools/src/reducers/index.js
@@ -1,26 +1,27 @@
 const scxml = require('../../../../..');
-const viz = document.getElementById("viz");
 import fnModel from '../../dist/drag-and-drop'
 
-const handleAction = (state, action) => {
+const initialState = { snapshot: null };
+
+const handleAction = ({ snapshot } = initialState, action) => {
   if(action.type && !action.name){
     action.name = action.type;
   }
   
   let interpreter
-  if(!state){
+  if(!snapshot){
     //instantiate the interpreter
     interpreter = new scxml.scion.Statechart(fnModel);
     //start the interpreter
     interpreter.start();
   }else{
     //instantiate the interpreter
-    interpreter = new scxml.scion.Statechart(fnModel, {snapshot : state});
+    interpreter = new scxml.scion.Statechart(fnModel, { snapshot });
   }
 
   interpreter.gen(action); 
 
-  return interpreter.getSnapshot();
+  return { snapshot: interpreter.getSnapshot() };
 }
 
 export default handleAction 
