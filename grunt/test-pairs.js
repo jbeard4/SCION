8b37f1e2f8151cd5f0888d153052ca061f8b21fd
Jacob Beard <jake@minnow.io>
2018-06-22 00:07:26 +0200

    Fixed tests for requirejs. Fixed bug expanding test pairs on mac. Ran tests in the browser, everything working well.

--- grunt/test-pairs.js
+++ grunt/test-pairs.js
@@ -1,7 +1,7 @@
 var grunt   = require('grunt');
 var path = require('path');
 
-var tests = grunt.file.expand(require('./scxml-tests.json').map( p => path.join(__dirname,'..',p)));
+var tests = grunt.file.expand(require('./scxml-tests.json').map( p => path.join(__dirname,'..',p))).map( fullPath => path.relative(__dirname, fullPath));
 
 var testPairs = tests.map(function(test){
   var filename = test.replace('\.scxml','.json');
