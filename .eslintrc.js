75eb5520e0883c5a1601cba8c7287f9a6690e380
Jacob Beard <jake@jacobeanrnd.com>
2018-09-01 18:34:32 -0400

    Some tests for eslint-plugin-sharpie

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 5
+    },
+    "plugins" : [
+        "scharpie"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
