69fb0613de682cb8f93ee06711056edff2358a18
Jacob Beard <jake@jacobeanrnd.com>
2018-09-02 21:04:52 -0400

    Updated package names and dependencies

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const scxml = require('scxml');
-require('scxml-sourcemap-plugin')(scxml);  //load the sourcemaps plugin
+const scxml = require('@scion-scxml/scxml');
+require('@scion-scxml/sourcemap-plugin')(scxml);  //load the sourcemaps plugin
 const diagnosticsClient = require('scion-scxml-monitor-middleware/client');
 
 diagnosticsClient.init(scxml); 
