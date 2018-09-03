ec6393ed4580a1b09dafd9aebe62854eca8ecf51
Jacob Beard <jake@jacobeanrnd.com>
2018-09-02 21:04:51 -0400

    Updated package names and dependencies

--- test/harness/node/index.js
+++ test/harness/node/index.js
@@ -1,5 +1,5 @@
 var scion = require('../../..');
-var tests = require('scion-core-test-framework')(scion.Statechart);
+var tests = require('@scion-scxml/core-test-framework')(scion.Statechart);
 
 Object.keys(tests).forEach(function(key){
   exports[key] = tests[key];
