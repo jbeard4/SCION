851c2b9580f742d46d1aeeda2129060a7e5151e3
Jacob Beard <jake@jacobeanrnd.com>
2018-09-06 16:25:52 -0400

    Fixed reference to scion-scxml/monitor-middleware/client. Added reference to licensezero

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const scxml = require('@scion-scxml/scxml');
 require('@scion-scxml/sourcemap-plugin')(scxml);  //load the sourcemaps plugin
-const diagnosticsClient = require('scion-scxml-monitor-middleware/client');
+const diagnosticsClient = require('@scion-scxml/monitor-middleware/client');
 
 diagnosticsClient.init(scxml); 
 
