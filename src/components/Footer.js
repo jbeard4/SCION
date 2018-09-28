75cee4ee5d2c55385b54a5a64b2046cb93dc4ce9
Jacob Beard <jake@jacobeanrnd.com>
2018-09-27 20:43:22 -0400

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -49,7 +49,7 @@ export default class Footer extends React.Component {
         right: 0
       }} 
       ref={ x => this.root = x }>
-      SCION is developed and maintained by <a href="https://jacobeanrnd.com">Jacobean Research and Development, LLC</a>.
+      SCION is developed and maintained by <a href="http://jacobeanrnd.com">Jacobean Research and Development, LLC</a>.
     </footer>
   }
 }
