4753bf4021ec05dc3558d45024bf86800793b25c
Jacob Beard <jake@minnow.io>
2021-04-18 10:13:01 -0600

    Add CSS to browserify build

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './containers/App';
 import configureStore from './store/configureStore';
+require("../node_modules/font-awesome/css/font-awesome.css")
+require("../node_modules/slickgrid/slick.grid.css")
+require("../node_modules/slickgrid/slick-default-theme.css")
+require("../node_modules/@scion-scxml/jsondiffpatch/public/formatters-styles/html.css")
 
 const store = configureStore();
 
