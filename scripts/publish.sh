52e14a39eb365a71da91925ee58a03c91dd5b14d
Jacob Beard <jake@jacobeanrnd.com>
2021-05-15 18:05:45 -0600

    Fix publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,1 +1,1 @@
-lerna exec --concurrency 1 "if [ \$( node -e \"console.log(require('./package.json').version)\" ) != \$( npm view . version ) ]; then npm publish; else echo package version up-to-date - skipping; fi;"
+node node_modules/.bin/lerna exec --concurrency 1 "if [ \$( node -e \"console.log(require('./package.json').version)\" ) != \$( npm view . version ) ]; then npm publish; else echo package version up-to-date - skipping; fi;"
