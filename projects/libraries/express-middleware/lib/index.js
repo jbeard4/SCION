8741bebf2907cbb7506131c3c650276f39110361
Jacob Beard <jake@jacobeanrnd.com>
2021-05-16 22:14:46 -0600

    Create simple REST API to init session and dispatch events on some session

--- projects/libraries/express-middleware/lib/index.js
+++ projects/libraries/express-middleware/lib/index.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const express = require('express')
 const monitorMiddlewareClient = require('@scion-scxml/monitor-middleware/client')
+const uuid = require('uuid')
+
+const mockDb = require('./mock-db')
 
 /*
  * options: 
@@ -50,5 +53,102 @@ module.exports = function ({
       res.write("data: " + JSON.stringify(messageData) + '\n\n'); // Note the extra newline
     }
   }
+
+  const modelCache = {}
+  const fnModelCache = {}
+
+  function initModel(scxmlName, cb){
+    scxml.pathToModel(`${pathToScxmlSrcDir}/${scxmlName}.scxml`, function(err, model){
+      if(err){ 
+        return cb(err);
+      }
+      modelCache[scxmlName] = model
+      model.prepare(function(err, fnModel) {
+        if(err){ 
+          return cb(err);
+        }
+        fnModelCache[scxmlName] = fnModel
+        return cb(null, fnModel)
+      })
+    })
+  }
+
+  function initModelOrFetchFromCache(scxmlName, cb){
+    if(!fnModelCache[scxmlName]){
+      initModel(scxmlName, (err, fnModel) => {
+        if(err) {
+          return cb(err)
+        }
+        cb(null, fnModel)
+      })
+    } else {
+      cb(null, fnModelCache[scxmlName])
+    }
+  }
+
+  function generateSessionid(){
+    return uuid.v4()
+  }
+
+
+  // API to init new session
+  app.post('/scion/:scxmlName', (req, res, next) => {
+    const scxmlName = req.params.scxmlName
+
+    initModelOrFetchFromCache(scxmlName, (err, fnModel) => {
+
+      if(err) throw err;
+
+      //instantiate the interpreter
+      const sc1 = new scxml.core.Statechart(fnModel, {generateSessionid});
+
+      sc1.start();
+
+      //save the snapshot to the database
+      const snapshot = sc1.getSnapshot()
+      const sessionId = sc1.opts.sessionid
+      mockDb.persistSession(sessionId, snapshot)
+
+      res.json({
+        sessionId,
+        snapshot 
+      })
+    })
+  });
+
+  // API to send event to session
+  app.post('/scion/:scxmlName/:sessionId', (req, res) => {
+
+    const scxmlName = req.params.scxmlName,
+      sessionId = req.params.sessionId;
+
+    const evt = req.body
+
+    console.log('evt ', evt )
+
+    //read the sessionId
+    // TODO: handle error where sessionId does not exist
+    const savedSnapshot = mockDb.getSession(sessionId)
+
+    //rehydrate the session
+    initModelOrFetchFromCache(scxmlName, (err, fnModel) => {
+      
+      if(err) throw err;
+
+      //instantiate the interpreter
+      const sc1 = new scxml.core.Statechart(fnModel, {generateSessionid, snapshot : savedSnapshot});
+
+      sc1.gen(evt)
+
+      //save the snapshot to the database
+      const newSnapshot = sc1.getSnapshot()
+      mockDb.persistSession(sessionId, newSnapshot)
+
+      res.json({
+        sessionId,
+        snapshot : newSnapshot
+      })
+    })
+  })
 }
 
